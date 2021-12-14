e485592fe0f06b599bda485e47ed9878a119fed8
Błażej Kochanek <kochanek.blazej@gmail.com>
2021-12-14 22:46:07 +0100

    Added list of all links to jobs offers

--- olx_scraper.py
+++ olx_scraper.py
@@ -1,11 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
+def offer_links(offers):
+    for offer in offers:
+        link_offer = offer.table.tr.td.div.h3.a.get("href")
+        offer_list.append(link_offer)
+    return offer_list
+
+
+def next_page(soup):
+    try:
+        page = requests.get(
+            soup.find("span", class_="fbold next abs large").a.get(
+                "href")).text
+        return page
+    except AttributeError:
+        return False
+
+def offer_details(offer_link):
+    pass
+
+offer_list = []
 url = requests.get("https://www.olx.pl/praca/informatyka/programista/").text
-soup = BeautifulSoup(url, "lxml")
 
-offers = soup.find_all("td", class_ = "title-cell title-cell--jobs")
+while url:
+    soup = BeautifulSoup(url, "lxml")
+    offers = soup.find_all("div", class_="offer-wrapper")
+    offer_links(offers)
+    url = next_page(soup)
 
-for offer in offers:
-    offer_name = offer.find("strong").text
-    print(offer_name)
+for offer_link in offer_list:
+    print(offer_link)
+
+
+
+print(len(offer_list))
